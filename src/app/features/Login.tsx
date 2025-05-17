02f4da7e5af3db5fbc3c9bf3db6c65d7a400dcea
vinay bajjuri <vbajjuri@altir.co>
2025-05-17 20:48:41 +0530

    Refactor : removed forgot password functionality

--- src/app/features/Login.tsx
+++ src/app/features/Login.tsx
@@ -173,7 +173,7 @@ export function LoginForm({
                   <div className="flex items-center">
                     <Label htmlFor="password">Password</Label>
                     <Link
-                      href="/reset-password"
+                      href="/login"
                       className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                     >
                       Forgot your password?
